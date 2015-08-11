b1259cb374ed787b7cb32700ca15c6e0bfed6a7b
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-11 14:38:52 +0200

    Add is-path?

--- src/bayes/net.clj
+++ src/bayes/net.clj
@@ -6,23 +6,44 @@
 ; :test
 
 (defn- alloc-node [id]
+  "Returns an empty node with `id`."
   {:id             id
-   :parent-id-list [] ; TODO: ordered by comparing pointers
-   :child-id-list  [] ; TODO: ordered by comparing pointers
+   :parent-id-list [] ; list of parent ids; TODO: ordered by comparing pointers
+   :child-id-list  [] ; list of child ids; TODO: ordered by comparing pointers
    :net-node-mark  :init})
 
-(defn alloc [n-node]
-  (map alloc-node (range n-node)))
+(defn alloc [n]
+  "Returns a net of `n` nodes."
+  (map alloc-node (range n)))
 
 (defn has-edge? [net from-id to-id]
   "Does `net` have an edge between the nodes with ids `from-id` and `to-id`?"
   (.contains (:parent-id-list (nth net to-id)) from-id))
 
-;(defn is-path? [net n parent] TODO)
+(defn is-path? [net from-id to-id]
+  "Returns true if there is a path from `from-id` to `to-id` in `net`."
+  (loop [queue   [from-id]
+         visited #{}]
+    (if (empty? queue)
+      false
+      (let [id (first queue)]
+        (if (= id to-id)
+          true
+          (recur
+            (reduce ; add children to queue if not visited
+              (fn [q i]
+                (if (not (.contains visited i))
+                  (conj q i)
+                  q))
+              queue
+              (:child-id-list (nth net id)))
+            (conj visited id)))))))
 
-;(defn- insert-edge [net parent n] TODO)
+;(defn- insert-edge [net from-id to-id] TODO)
 
 (defn generate-random-edges [net max-num-parent percent-parent]
+  "Extends `net` with random edges, maximally `max-num-parent` for each node
+  with a chance of `percent-parent`."
   (for [n (range (count net))
         p (range max-num-parent)]
     (if (< (rand-int 100) percent-parent)
