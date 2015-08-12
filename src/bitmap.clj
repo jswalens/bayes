d7897a3596c4ac6503e0a30ad1fe8a8128c310f6
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-12 12:55:08 +0200

    Add bitmap, which should have been added some commits ago.

---
+++ src/bitmap.clj
@@ -0,0 +1,18 @@
+(ns bitmap)
+
+(defn create [length]
+  "Create a bitmap of `length` falses."
+  (vec (repeat length false)))
+
+(defn set [bitmap i]
+  "Set bit `i` in `bitmap` to true."
+  (assoc bitmap i true))
+
+(defn is-set? [bitmap i]
+  "Returns true if bit `i` in `bitmap` is true."
+  (= (nth bitmap i) true))
+
+(defn find-clear [bitmap start-index]
+  "Returns the index of the first false element in `bitmap`, after
+  `start-index`. Returns nil if all bits are true."
+  (first (filter false? (drop start-index bitmap))))
