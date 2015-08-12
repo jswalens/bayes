30e3f5d52e071cb5c9c28d094366074cba723aea
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-12 12:35:26 +0200

    Finish this part of data/generate, might be refactorable.

--- src/bayes/data.clj
+++ src/bayes/data.clj
@@ -1,5 +1,6 @@
 (ns bayes.data
   (:require [clojure.math.numeric-tower :as math]
+            [bitmap]
             [bayes.net :as net]))
 
 (def ^:const DATA_PRECISION 100)
@@ -25,35 +26,43 @@
         thresholds
           (for [v (range (:n-var data))]
             (for [t (range (math/expt 2 (count (net/get-parent-id-list net v))))]
-              (rand-int (inc DATA_PRECISION))))]
-    ; Create variable dependency ordering for record generation
-    #_(loop [order        []
-           queue        []
-           dependencies []
-           ordered      ...
-           done         ...]
-      (let [v (bitmap/find-clear done (vorige v + 1))]
-        (if (< v 0)
-          ...break out of loop...
-          (if (not= (count (net/get-child-id-list net v)) 0)
-            (recur order queue dependencies ordered done)
-            (let [; Use breadth-first search to find net connected to this leaf
-                  [new-queue updated-done updated-dependencies]
-                    (reduce
-                      (fn [{q :queue don :done dep :dependencies} id]
-                        {:queue
+              (rand-int (inc DATA_PRECISION))))
+        ; Create variable dependency ordering for record generation
+        order
+          (loop [order   []
+                 queue   []
+                 ordered (bitmap/create (:n-var data))
+                 done    (bitmap/create (:n-var data))]
+            (let [v (bitmap/find-clear done (vorige v + 1))]
+              (if (< v 0)
+                order
+                (if (not= (count (net/get-child-id-list net v)) 0)
+                  (recur order queue dependencies ordered done)
+                  (let [; Use breadth-first search to find net connected to this leaf
+                        {new-queue :queue updated-done :done dependencies :dependencies}
                           (reduce
-                            (fn [q_ parent-id]
-                              (conj q_ parent-id))
-                            q
-                            (net/get-parent-id-list net id))
-                         :done         ...set id to true in done...
-                         :dependencies (conj dep id)})
-                      {:queue [] :done done :dependencies dependencies}
-                      queue)
-                  ; Create ordering
-                  ...]
-              (recur ...))))))
+                            (fn [{q :queue don :done dep :dependencies} id]
+                              {:queue
+                                (reduce
+                                  (fn [q_ parent-id]
+                                    (conj q_ parent-id))
+                                  q
+                                  (net/get-parent-id-list net id))
+                               :done         (bitmap/set done id)
+                               :dependencies (conj dep id)})
+                            {:queue [] :done done :dependencies []}
+                            queue)
+                        ; Create ordering
+                        [updated-order updated-ordered]
+                          (reduce
+                            (fn [[order_ ordered_] dep]
+                              (if (not (bitmap/is-set? ordered_ dep))
+                                [(conj order dep)
+                                 (bitmap/set ordered_ dep)]
+                                [order_ ordered_]))
+                            [order ordered]
+                            dependencies)]
+                    (recur updated-order new-queue updated-ordered updated-done))))))]
     ; Create records
     ; TODO
     ; Clean up
