d7da86de7044b7bf1b473a84b93070fb6845877d
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-14 11:45:24 +0200

    Simplify: n-record is also in data.

--- src/bayes/adtree.clj
+++ src/bayes/adtree.clj
@@ -4,12 +4,12 @@
 (declare make-vary)
 (declare make-node)
 
-(defn- make-vary [parent-i i start n-record data]
-  (let [data (if (and (not= (inc parent-i) i) (> n-record 1))
-                (data/sort data start n-record i)
+(defn- make-vary [parent-i i start data]
+  (let [data (if (and (not= (inc parent-i) i) (> (:n-record data) 1))
+                (data/sort data i) ; TODO: not passing start is that ok?
                 data)
         num0 (data/find-split data start i)
-        num1 (- n-record num0)
+        num1 (- (:n-record data) num0)
         most-common-value (if (>= num0 num1) 0 1)]
     {:index             i
      :most-common-value most-common-value
@@ -24,15 +24,15 @@
                             (make-node i i (+ start num0) num1 data)
                             (assoc :value 1)))}))
 
-(defn- make-node [parent-i i start n-record data]
+(defn- make-node [parent-i i start data]
   {:index       i
    :value       -1
-   :count       n-record
+   :count       (:n-record data)
    :vary-vector (for [v (range (inc i) (:n-var data))]
-                  (make-vary parent-i v start n-record data))})
+                  (make-vary parent-i v start data))})
 
 (defn make [data]
   "Make ADTree (alternating decision tree) based on `data`."
   {:n-var     (:n-var data)
    :n-record  (:n-record data)
-   :root-node (make-node -1 -1 0 (:n-record data) (data/sort data 0))})
+   :root-node (make-node -1 -1 0 (data/sort data 0))})
