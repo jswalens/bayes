5a04db0ef3f98b56521c4c724f255bf58e1c5215
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-14 13:26:51 +0200

    Revert commit "Simplify: n-record is also in data." (d7da86de7044b7bf1b473a84b93070fb6845877d).
    n is not always equal to (:n-record data), as recursive calls to make-node in make-vary modify this parameter.

--- src/bayes/adtree.clj
+++ src/bayes/adtree.clj
@@ -4,35 +4,35 @@
 (declare make-vary)
 (declare make-node)
 
-(defn- make-vary [parent-i i start data]
-  (let [data (if (and (not= (inc parent-i) i) (> (:n-record data) 1))
-                (data/sort data i) ; TODO: not passing start is that ok?
+(defn- make-vary [parent-i i start n data]
+  (let [data (if (and (not= (inc parent-i) i) (> n 1))
+                (data/sort data i) ; not passing start or n: is this ok?
                 data)
-        num0 (data/find-split data start i)
-        num1 (- (:n-record data) num0)
-        most-common-value (if (>= num0 num1) 0 1)]
+        n-0  (data/find-split data start i) ; number of 0s
+        n-1  (- n n-0)                      ; number of 1s
+        most-common-value (if (>= n-0 n-1) 0 1)]
     {:index             i
      :most-common-value most-common-value
-     :zero-node         (if (or (= num0 0) (= most-common-value 0))
+     :zero-node         (if (or (= n-0 0) (= most-common-value 0))
                           nil
                           (->
-                            (make-node i i start num0 data)
+                            (make-node i i start n-0 data)
                             (assoc :value 0)))
-     :one-node          (if (or (= num1 0) (= most-common-value 1))
+     :one-node          (if (or (= n-1 0) (= most-common-value 1))
                           nil
                           (->
-                            (make-node i i (+ start num0) num1 data)
+                            (make-node i i (+ start n-0) n-1 data)
                             (assoc :value 1)))}))
 
-(defn- make-node [parent-i i start data]
+(defn- make-node [parent-i i start n data]
   {:index       i
    :value       -1
-   :count       (:n-record data)
+   :count       n
    :vary-vector (for [v (range (inc i) (:n-var data))]
-                  (make-vary parent-i v start data))})
+                  (make-vary parent-i v start n data))})
 
 (defn make [data]
   "Make ADTree (alternating decision tree) based on `data`."
   {:n-var     (:n-var data)
    :n-record  (:n-record data)
-   :root-node (make-node -1 -1 0 (data/sort data 0))})
+   :root-node (make-node -1 -1 0 (:n-record data) (data/sort data 0))})
