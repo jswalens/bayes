5066302d772b15f6d46a870f5e0279c19a44b3dc
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-14 15:15:37 +0200

    Start working on learner/score.

--- src/bayes/learner.clj
+++ src/bayes/learner.clj
@@ -1,6 +1,8 @@
 (ns bayes.learner
   (:require [bayes.net :as net]))
 
+(def ^:const QUERY_VALUE_WILDCARD -1)
+
 (defn alloc [data adtree]
   "Allocate the learner.
 
@@ -14,5 +16,49 @@
    :task-list                  (list) ; TODO: sorted by compareTask
    :n-total-parent             0})
 
+(defn- populate-query-vectors [net id queries]
+  "TODO"
+  [[] []])
+
+(defn- compute-local-log-likelihood [id adtree queries query-vector parent-query-vector]
+  "TODO"
+  [0.0 []])
+
+(defn- sum [ns]
+  "Sums `ns`."
+  (reduce + ns))
+
 (defn score [learner]
-  0)
+  (let [n-var
+          (:n-var (:adtree learner))
+        initial-queries
+          (for [v (range n-var)]
+            {:index v :value QUERY_VALUE_WILDCARD})
+        n-total-parent
+          (sum
+            (map
+              (fn [v] (count (net/get-parent-id-list (:net learner) v)))
+              (range n-var)))
+        log-likelihood
+          (first
+            (reduce
+              (fn [[log-likelihood queries] v]
+                (let [[query-vector parent-query-vector]
+                        (populate-query-vectors (:net learner) v queries)
+                      [local-log-likelihood updated-queries]
+                        (compute-local-log-likelihood
+                          v
+                          (:adtree learner)
+                          queries
+                          query-vector
+                          parent-query-vector)]
+                [(+ log-likelihood local-log-likelihood) updated-queries]))
+              [0 initial-queries]
+              (range n-var)))
+        n-record
+          (:n-record (:adtree learner))
+        penalty
+          (* -0.5 n-total-parent (Math/log n-record))
+        score
+          (+ penalty (* n-record log-likelihood))]
+    score))
