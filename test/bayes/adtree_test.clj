8f04a821d41826ed9241aaf478327cdd17946c3b
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-21 14:53:42 +0200

    Add test for adtree/get-count.

--- test/bayes/adtree_test.clj
+++ test/bayes/adtree_test.clj
@@ -1,6 +1,12 @@
 (ns bayes.adtree-test
   (:require [clojure.test :refer :all]
-            [bayes.adtree]))
+            [bayes.adtree :as adtree]))
+
+(def example-data
+  {:n-var 2 :n-record 4 :records [[0 0] [0 1] [1 0] [1 1]]})
+
+(def example-adtree
+  (adtree/make example-data))
 
 ; Note: @#'bayes.adtree/drop-one is a trick to get the private function drop-one
 
@@ -15,3 +21,30 @@
 (deftest swap-bit
   (is (= 0 (@#'bayes.adtree/swap-bit 1)))
   (is (= 1 (@#'bayes.adtree/swap-bit 0))))
+
+(deftest get-count
+  ; TODO: I don't know whether these are the expected results
+  (is (= 2 (adtree/get-count example-adtree
+    [{:index 0 :value 0}]
+    [0])))
+  (is (= 2 (adtree/get-count example-adtree
+    [{:index 0 :value 1}]
+    [0])))
+  (is (= 2 (adtree/get-count example-adtree
+    [{:index 0 :value 0} {:index 1 :value 1}]
+    [0])))
+  (is (= 2 (adtree/get-count example-adtree
+    [{:index 0 :value 0} {:index 1 :value 1}]
+    [1])))
+  (is (= 2 (adtree/get-count example-adtree
+    [{:index 0 :value 1} {:index 1 :value 1}]
+    [0])))
+  (is (= 2 (adtree/get-count example-adtree
+    [{:index 0 :value 1} {:index 1 :value 1}]
+    [1])))
+  (is (= 1 (adtree/get-count example-adtree
+    [{:index 0 :value 0} {:index 1 :value 1}]
+    [0 1])))
+  (is (= 4 (adtree/get-count example-adtree
+    [{:index 0 :value 0} {:index 1 :value 1}]
+    []))))
