29835324df4997c9bc3c80235c943aab2f9eb6a2
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-21 14:10:09 +0200

    Test sort-queries and set-query-value.

--- test/bayes/learner_test.clj
+++ test/bayes/learner_test.clj
@@ -20,3 +20,14 @@
   (let [tasks (ref [{:op 1 :score 1} {:op 2 :score 2} {:op 3 :score 3}])]
     (is (= {:op 3 :score 3} (@#'bayes.learner/pop-task tasks)))
     (is (= [{:op 1 :score 1} {:op 2 :score 2}] @tasks))))
+
+(deftest sort-queries
+  (is (= [{:index 0 :value 10} {:index 1 :value 11}]
+    (@#'bayes.learner/sort-queries [{:index 1 :value 11} {:index 0 :value 10}]))))
+
+(deftest set-query-value
+  (is (= [{:index 0 :value 10} {:index 1 :value 21} {:index 2 :value 12}]
+    (@#'bayes.learner/set-query-value
+      [{:index 0 :value 10} {:index 1 :value 11} {:index 2 :value 12}]
+      1
+      21))))
