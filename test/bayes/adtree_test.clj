35140785c01123e8b474b4edacb409dfb7dde490
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-21 11:24:37 +0200

    Test drop-one.

---
+++ test/bayes/adtree_test.clj
@@ -0,0 +1,13 @@
+(ns bayes.adtree-test
+  (:require [clojure.test :refer :all]
+            [bayes.adtree]))
+
+; Note: @#'bayes.adtree/drop-one is a trick to get the private function drop-one
+
+(deftest drop-one
+  (are [coll i expected] (= expected (@#'bayes.adtree/drop-one i coll))
+    [0 1 2] 0 [1 2]
+    [0 1 2] 1 [0 2]
+    [0 1 2] 2 [0 1]
+    [0 1 2] 3 [0 1 2]
+    []      0 []))
