83c0d1243f3a94ae775d96b806532c30844aef4a
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-24 15:20:16 +0200

    Test adtree/make against C version.

--- test/bayes/adtree_test.clj
+++ test/bayes/adtree_test.clj
@@ -2,11 +2,105 @@
   (:require [clojure.test :refer :all]
             [bayes.adtree :as adtree]))
 
-(def example-data
+(def example-data-2
   {:n-var 2 :n-record 4 :records [[0 0] [0 1] [1 0] [1 1]]})
 
-(def example-adtree
-  (adtree/make example-data))
+(def example-adtree-2
+  (adtree/make example-data-2))
+
+(def expected-adtree-2
+  ; Retrieved from C version
+  {:n-var 2
+   :n-record 4
+   :root-node
+     {:index -1
+      :value -1
+      :count 4
+      :vary-vector (list
+        {:index 0
+         :most-common-value 0
+         :zero-node nil
+         :one-node
+           {:index 0
+            :value 1
+            :count 2
+            :vary-vector (list
+              {:index 1
+               :most-common-value 0
+               :zero-node nil
+               :one-node
+                 {:index 1
+                  :value 1
+                  :count 1
+                  :vary-vector (list)}})}}
+        {:index 1
+         :most-common-value 0
+         :zero-node nil
+         :one-node
+           {:index 1
+            :value 1
+            :count 2
+            :vary-vector (list)}})}})
+
+(def example-data-3
+  {:n-var 3 :n-record 4 :records [[1 0 0] [1 0 1] [0 0 0] [0 1 0]]})
+
+(def example-adtree-3
+  (adtree/make example-data-3))
+
+(def expected-adtree-3
+  ; Retrieved from C version
+  {:n-var 3
+   :n-record 4
+   :root-node
+     {:index -1
+       :value -1
+       :count 4
+       :vary-vector (list
+         {:index 0
+          :most-common-value 0
+          :zero-node nil
+          :one-node
+            {:index 0
+             :value 1
+             :count 2
+             :vary-vector (list
+               {:index 1
+                :most-common-value 0
+                :zero-node nil
+                :one-node nil}
+               {:index 2
+                :most-common-value 0
+                :zero-node nil
+                :one-node
+                  {:index 2
+                   :value 1
+                   :count 1
+                   :vary-vector (list)}})}}
+         {:index 1
+          :most-common-value 0
+          :zero-node nil
+          :one-node
+            {:index 1
+             :value 1
+             :count 1
+             :vary-vector (list
+               {:index 2
+                :most-common-value 0
+                :zero-node nil
+                :one-node nil})}}
+         {:index 2
+          :most-common-value 0
+          :zero-node nil
+          :one-node
+            {:index 2
+             :value 1
+             :count 1
+             :vary-vector (list)}})}})
+
+(deftest make
+  (is (= expected-adtree-2 example-adtree-2))
+  (is (= expected-adtree-3 example-adtree-3)))
 
 ; Note: @#'bayes.adtree/drop-one is a trick to get the private function drop-one
 
@@ -24,27 +118,27 @@
 
 (deftest get-count
   ; TODO: I don't know whether these are the expected results
-  (is (= 2 (adtree/get-count example-adtree
+  (is (= 2 (adtree/get-count example-adtree-2
     [{:index 0 :value 0}]
     [0])))
-  (is (= 2 (adtree/get-count example-adtree
+  (is (= 2 (adtree/get-count example-adtree-2
     [{:index 0 :value 1}]
     [0])))
-  (is (= 2 (adtree/get-count example-adtree
+  (is (= 2 (adtree/get-count example-adtree-2
     [{:index 0 :value 0} {:index 1 :value 1}]
     [0])))
-  (is (= 2 (adtree/get-count example-adtree
+  (is (= 2 (adtree/get-count example-adtree-2
     [{:index 0 :value 0} {:index 1 :value 1}]
     [1])))
-  (is (= 2 (adtree/get-count example-adtree
+  (is (= 2 (adtree/get-count example-adtree-2
     [{:index 0 :value 1} {:index 1 :value 1}]
     [0])))
-  (is (= 2 (adtree/get-count example-adtree
+  (is (= 2 (adtree/get-count example-adtree-2
     [{:index 0 :value 1} {:index 1 :value 1}]
     [1])))
-  (is (= 1 (adtree/get-count example-adtree
+  (is (= 1 (adtree/get-count example-adtree-2
     [{:index 0 :value 0} {:index 1 :value 1}]
     [0 1])))
-  (is (= 4 (adtree/get-count example-adtree
+  (is (= 4 (adtree/get-count example-adtree-2
     [{:index 0 :value 0} {:index 1 :value 1}]
     []))))
