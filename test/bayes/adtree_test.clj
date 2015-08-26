5f2c8c0188be389ec6052ccefe7b6376628a77f6
Janwillem Swalens <jswalens@vub.ac.be>
2015-08-26 13:46:35 +0200

    Add case that triggers error in code.

--- test/bayes/adtree_test.clj
+++ test/bayes/adtree_test.clj
@@ -98,9 +98,62 @@
              :count 1
              :vary-vector (list)}})}})
 
+(def example-data-4
+  {:n-var 3 :n-record 2 :records [[0 0 0] [1 1 1]]})
+
+(def example-adtree-4
+  (adtree/make example-data-4))
+
+(def expected-adtree-4
+  ; Retrieved from C version
+  {:n-var 3
+   :n-record 2
+   :root-node
+     {:index -1
+      :value -1
+      :count 2
+      :vary-vector (list
+        {:index 0
+         :most-common-value 0
+         :zero-node nil
+         :one-node
+           {:index 0
+            :value 1
+            :count 1
+            :vary-vector (list
+              {:index 1
+               :most-common-value 1
+               :zero-node nil
+               :one-node nil}
+              {:index 2
+               :most-common-value 1
+               :zero-node nil
+               :one-node nil})}}
+        {:index 1
+         :most-common-value 0
+         :zero-node nil
+         :one-node
+           {:index 1
+            :value 1
+            :count 1
+            :vary-vector (list
+              {:index 2
+               :most-common-value 1
+               :zero-node nil
+               :one-node nil})}}
+        {:index 2
+         :most-common-value 0
+         :zero-node nil
+         :one-node
+           {:index 2
+            :value 1
+            :count 1
+            :vary-vector (list)}})}})
+
 (deftest make
   (is (= expected-adtree-2 example-adtree-2))
-  (is (= expected-adtree-3 example-adtree-3)))
+  (is (= expected-adtree-3 example-adtree-3))
+  (is (= expected-adtree-4 example-adtree-4)))
 
 ; Note: @#'bayes.adtree/drop-one is a trick to get the private function drop-one
 
@@ -147,4 +200,8 @@
     [0 1])))
   (is (= 0 (adtree/get-count example-adtree-3
     [{:index 0 :value 1} {:index 0 :value 1}]
-    [0 1]))))
+    [0 1])))
+  ; The following causes an error in the code (super count <= invert count)
+  (is (= 0 (adtree/get-count example-adtree-4
+    [{:index 0 :value 0} {:index 0 :value 1} {:index 0 :value 0}]
+    [0 1 2]))))
