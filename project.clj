bf8267314747b15e9d79902131ce60de0fd45730
Janwillem Swalens <jswalens@vub.ac.be>
2015-09-02 15:17:43 +0200

    Update timbre version.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "STAMP Bayes in Clojure"
   :url "http://example.com/TODO"
   :dependencies [[org.clojure/math.numeric-tower "0.0.4"]
-                 [com.taoensso/timbre "3.4.0"]]
+                 [com.taoensso/timbre "4.1.1"]]
   :resource-paths ["resources/clojure-1.6.0.jar"]
   :main ^:skip-aot bayes.main
   :target-path "target/%s"
