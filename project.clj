8864c82e8f3833e7ec10eac95801d167079e6b39
Janwillem Swalens <jswalens@vub.ac.be>
2015-09-02 13:45:12 +0200

    Add "cloverage" to assess test coverage. Only works with original Clojure, not with my version that has transactional futures. (Because it uses clojure/tools.logging, which calls (clojure.lang.LockingTransaction/isRunning), a function that does not exist in my implementation.)

--- project.clj
+++ project.clj
@@ -6,4 +6,11 @@
   :resource-paths ["resources/clojure-1.6.0.jar"]
   :main ^:skip-aot bayes.main
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {
+    :dev {
+      :plugins [[lein-cloverage "1.0.6"]]
+    }
+    :uberjar {
+      :aot :all
+    }
+  })
