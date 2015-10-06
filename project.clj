faa5df73d6e5da0eb58ef2c03c2d2fe2905de392
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-06 15:17:51 +0200

    Make sure correct version of Clojure is always used, also in libraries.

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject bayes "0.1.0-SNAPSHOT"
   :description "STAMP Bayes in Clojure"
   :url "http://example.com/TODO"
-  :dependencies [[org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [com.taoensso/timbre "4.1.4"]]
+  ; Note: exclude org.clojure/clojure everywhere to make sure we're using our
+  ; version of Clojure all the time.
+  :dependencies [[org.clojure/math.numeric-tower "0.0.4" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.cli "0.3.3" :exclusions [org.clojure/clojure]]
+                 [com.taoensso/timbre "4.1.4" :exclusions [org.clojure/clojure]]]
   :resource-paths ["resources/clojure-1.6.0.jar"]
-  :main ^:skip-aot bayes.main
+  :main bayes.main
   :target-path "target/%s"
   :profiles {
     :dev {
