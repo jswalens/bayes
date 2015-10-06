47f71c6b4d5bd8dae6b8dc7e1ec64082e750d1c7
Janwillem Swalens <jswalens@vub.ac.be>
2015-10-06 11:33:23 +0200

    Update version of timbre.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/TODO"
   :dependencies [[org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.cli "0.3.3"]
-                 [com.taoensso/timbre "4.1.1"]]
+                 [com.taoensso/timbre "4.1.4"]]
   :resource-paths ["resources/clojure-1.6.0.jar"]
   :main ^:skip-aot bayes.main
   :target-path "target/%s"
