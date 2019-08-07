98011c89781063486a2bdda60123242f0622f5f8
Janwillem Swalens <jswalens@vub.be>
2019-08-07 14:30:56 +0200

    Update to Chocola 2.0.0, instead of using our old fork of Clojure. Also update version number in project.clj, last commit was tagged v9.

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject bayes "0.1.0-SNAPSHOT"
-  :description "STAMP Bayes in Clojure"
-  :url "http://example.com/TODO"
-  ; Note: exclude org.clojure/clojure everywhere to make sure we're using our
-  ; version of Clojure all the time.
-  :dependencies [[org.clojure/math.numeric-tower "0.0.4" :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.cli "0.3.3" :exclusions [org.clojure/clojure]]
-                 [com.taoensso/timbre "4.1.4" :exclusions [org.clojure/clojure]]]
-  :resource-paths ["resources/clojure-1.6.0.jar"]
+(defproject bayes "10.0.0"
+  :description "Bayes benchmark from STAMP in Clojure, using transactional futures."
+  :url "http://soft.vub.ac.be/~jswalens/chocola/"
+  :dependencies [[org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [com.taoensso/timbre "4.1.4"]]
+  :resource-paths ["resources/chocola-2.0.0-standalone.jar"]
+  :injections [(require 'chocola.core)]
   :main bayes.main
-  :target-path "target/%s"
   :profiles {
     :dev {
       :plugins [[lein-cloverage "1.0.6"]]
